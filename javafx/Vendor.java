fc9a084e6a8eaad4ee4a9a61792cf08d81536c14
Khaled Mohamed <167908428+KHMO02@users.noreply.github.com>
2024-05-09 05:35:56 +0300

    Add files via upload

---
+++ javafx/Vendor.java
@@ -0,0 +1,47 @@
+package javafx;
+
+import javafx.stage.Stage;
+
+public class Vendor extends User{
+    private Product products[]=new Product[20];
+    private int NumOfProducts;
+    
+    public Vendor(String name, String password) {
+        super(name,password);
+
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public int getNumOfProducts() {
+        return NumOfProducts;
+    }
+
+    public void setNumOfProducts(int NumOfProducts) {
+        this.NumOfProducts = NumOfProducts;
+    }
+    
+    
+    public void addProduct(Product p){
+        products[NumOfProducts]=p;
+        NumOfProducts++;
+    }
+    
+    
+
+
+
+    @Override
+    public String toString() {
+        return "Name: " + super.getName() + " ID: " + super.getID();
+    }
+    
+    @Override
+    public void openPage(){
+        SellerPage sellerpage=new SellerPage();
+        sellerpage.start(new Stage());
+    }
+    
+}
