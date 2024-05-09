1b398c309676ea3d7a34f8104b303fde529bbf98
Khaled Mohamed <167908428+KHMO02@users.noreply.github.com>
2024-05-09 05:32:17 +0300

    Add files via upload

---
+++ javafx/Customer.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafx;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author genzoo
+ */
+public class Customer extends User {
+private String Phone=null;
+private String Email=null;
+private String Address=null;
+private Order order;
+ 
+
+ 
+      public Customer(String name, String password) {
+        super(name, password);
+        order=new Order();
+        SystemManager.addOrder(order);
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+    
+    
+    
+    @Override
+    public void ShowInformation() {
+    }
+    
+@Override
+    public void openPage(){
+        CustomerPage CustomerPage=new CustomerPage();
+        CustomerPage.start(new Stage());
+    }
+}
